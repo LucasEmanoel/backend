06705d8b052234792c94c83528abe7e30d94212a
LucasEmanoel <lucas2014.barros@gmail.com>
2019-06-16 17:01:24 -0300

    dd github

---
+++ src/models/Post.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
+
+const PostScheme = new mongoose.Schema({
+  author: String,
+  place: String,
+  description: String,
+  hashtags: String,
+  image: String,
+  likes: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Post', PostScheme);
